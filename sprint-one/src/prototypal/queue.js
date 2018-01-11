115b16e81da375082e1564ce7b30056e27d5ab5b
jorgeC0218 <jorgecc0218@gmail.com>
2018-01-11 16:25:05 -0600

    solved stack and queue prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
+
 var Queue = function() {
+  var instance = Object.create(queueMethods);
+  instance.front = 0;
+  instance.end = 0;
+  instance.storage = {};
+
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+enqueue: function(value){
+  this.storage[this.end]= value;
+  this.end++;
+},
+dequeue: function(){
+  if(this.front <= this.end){
+    var value = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return value;
+    }
+  },
+  size: function(){
+    if(this.front - this.end < 0){
+    return this.end - this.front;
+  } else{
+    return 0;
+  }
+ }
+};
