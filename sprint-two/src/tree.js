5dc9ca85e50d849e48c4ef3f6134a29dd7b93892
jorgeC0218 <jorgecc0218@gmail.com>
2018-01-13 11:17:35 -0600

    solved tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,65 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-  newTree.children = null;
-  newTree.addChild = [];  // fix me
-  
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a child
+  var newChild = new Tree(value);
+  //push into children array
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  if(this.value === target){
+    return true;
+  }
+  //loop through the children array
+  for(var i = 0; i < this.children.length; i++){
+    //compare if this value is equal to target
+    var child = this.children[i];
+    if(child.contains(target)){
+      //return true
+      return true;
+    }
+  }
+
+  return false;
+
+};
+/*var Tree = function(value){
+  var newTree = {};
+  newTree.value = value;
+  newTree.addChild = [];  // fix me
+  _.extend(newTree, treeMethods);
+  return newTree;
 };
 
+var treeMethods = {};
+treeMethods.addChild = function(value){
+  var newChil = new Tree(value);
+  this.addChild.push(newChil);
+};
 
-
+treeMethods.contains = function(target) {
+  if(this.value === target){
+    return true;
+  }
+  for(var i = 0; i < this.children.length; i++){
+    var child = this.addChild[i];
+    if(child.contains(target)){
+      return true;
+    }
+    return false;
+  }
+  return false;
+};
+*/
 /*
  * Complexity: What is the time complexity of the above functions?
  */
